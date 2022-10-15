31c2e6342a717a92d008fa98584ac4c46851a235
donghyeon.seo <sdh20282@gmail.com>
2022-10-15 17:09:57 +0900

    update 실습

--- 실습/1만 시간의 법칙/기본/js/10000hours.js
+++ 실습/1만 시간의 법칙/기본/js/10000hours.js
@@ -1,1 +1,30 @@
 // js file
+
+function isEmpty(str) {
+    return !str.trim().length;
+}
+
+const calc_start = document.querySelector('#calc_start');
+const user_input_job = document.querySelector('#user_input_job');
+const user_input_time = document.querySelector('#user_input_time');
+const user_output_job = document.querySelector('#user_output_job');
+const user_output_time = document.querySelector('#user_output_time');
+
+const calc_result = document.querySelector('.specialist_result');
+const loading = document.querySelector('.loading');
+
+calc_start.addEventListener('click', function () {
+    if (!isEmpty(user_input_job.value) && !isEmpty(user_input_time.value)) {
+        calc_result.style.display = "none";
+
+        setTimeout(() => {
+            user_output_job.textContent = user_input_job.value;
+            user_output_time.textContent = Math.floor(10000 / parseInt(user_input_time.value));
+            loading.style.display = "none";
+            calc_result.style.display = "block";
+        }, 2000);
+
+        loading.style.display = "block";
+    }
+});
+
