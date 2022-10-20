92c62072336c115fd73fad78e5181443fff152ae
donghyeon.seo <sdh20282@gmail.com>
2022-10-20 22:49:22 +0900

    update Pet_SNS

---
+++ 실습/Pet_SNS/module/jwtMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const jwtMiddleware = async (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) {
+    res.locals.isAuthenticated = {};
+    return next();
+  }
+
+  try {
+    const decoded = jwt.verify(token, "secret");
+    req.userInfo = {
+      _id: decoded._id,
+      username: decoded.username,
+    };
+    res.locals.isAuthenticated = { username: decoded.username };
+    return next();
+  } catch (error) {
+    res.status(500).send("jwt error!");
+  }
+};
+
+module.exports = jwtMiddleware;
